1f223f7716615ecf5adf1de62d72179709d8de61
Kauã Neves <131424852+DevKneves@users.noreply.github.com>
2024-04-26 17:03:57 -0300

    Add files via upload

---
+++ AreaTriangulo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+            Console.WriteLine("Entre com as medidas do Triangulo X:");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do Triangulo Y:");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            //uso da formula de Heron
+            double p = (xA + xB + xC) / 2.0;
+            double areaX = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+            p = (yA + yB + yC) / 2.0;
+            double areaY = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("area de X: " + areaX.ToString("F4"));
+            Console.WriteLine("area de Y: " + areaY.ToString("F4"));
+
+            if (areaX > areaY)
+                Console.WriteLine("Área X é Maior");
+            else
+                Console.WriteLine("Área Y é Maior");
+        }
+    }
+}
